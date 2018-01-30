63bf015a2de2973031729fa63cbec525b457a55f
avinash.maurya <avinash.maurya@ril.com>
2018-01-30 11:29:54 +0530

    Add AWS backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
-let backendUrl = "http://localhost:4000/";
+// let backendUrl = "http://localhost:4000/";
 // let backendUrl = "https://api-word-app.herokuapp.com/";
+let backendUrl = "http://35.164.203.209:4000/";
 
 module.exports = {
     backendUrl
