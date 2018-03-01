4b787aa8569faf659f737e065e5785758a279c1f
avinash.maurya <avinash.maurya@ril.com>
2018-03-01 22:39:04 +0530

    Hosting build folder on heroku

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@
 // let backendUrl = "http://35.164.203.209:4000/"; // AWS Instance ID
 // let backendUrl = "http://ec2-54-148-0-172.us-west-2.compute.amazonaws.com:4000/"
 // let backendUrl = "https://us-central1-words-book.cloudfunctions.net/app/"
-let backendUrl = "https://avinashmaurya.xyz/api-word-app";
+let backendUrl = "https://avinashmaurya.xyz/api-word-app/";
 
 module.exports = {
     backendUrl
